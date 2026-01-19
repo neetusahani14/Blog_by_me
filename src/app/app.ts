fe7706048455067f8a9f2ce87fdf9d153b1f2684
neetusahani14 <neetusahani78226@gmail.com>
2026-01-19 17:07:03 +0530

    yes commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./Layouts/header/header";
+import { CategoryNavbar } from './Layouts/category-navbar/category-navbar';
+import { Footer } from './Layouts/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, CategoryNavbar,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
