d80f4d92e745232694a711ee4747290a53b006ec
neetusahani14 <neetusahani78226@gmail.com>
2026-01-19 20:40:34 +0530

    commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./Layouts/header/header";
 import { CategoryNavbar } from './Layouts/category-navbar/category-navbar';
 import { Footer } from './Layouts/footer/footer';
+import { PostCard } from "./Layouts/post-card/post-card";
+import { SubscriptionForm } from "./subscription-form/subscription-form";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, CategoryNavbar,Footer],
+  imports: [RouterOutlet, Header, CategoryNavbar, Footer, PostCard, SubscriptionForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
