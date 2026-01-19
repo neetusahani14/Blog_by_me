d2b202a363e46d84bf57cb8e88477281732d4467
neetusahani14 <neetusahani78226@gmail.com>
2026-01-19 16:24:56 +0530

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { SingleCategory } from './pages/single-category/single-category';
+import { ContactUs } from './pages/contact-us/contact-us';
+import { AboutUs } from './pages/about-us/about-us';
+import { TermsAndConditions } from './pages/terms-and-conditions/terms-and-conditions';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Home},
+    {path:'category', component:SingleCategory},
+    {path:'about', component:AboutUs},
+    {path:'contact', component:ContactUs},
+    {path:'trem-conditions', component:TermsAndConditions}
+];
---
+++ src/app/subscription-form/subscription-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription-form',
+  imports: [],
+  templateUrl: './subscription-form.html',
+  styleUrl: './subscription-form.css',
+})
+export class SubscriptionForm {
+
+}
